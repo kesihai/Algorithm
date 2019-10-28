a0829823a3a9b99bf2f125e93c7b96ae8fb29d43
kesihai <2693711535@qq.com>
2019-10-28 21:24:05 +0900

    add more spide window

---
+++ src/main/java/slidwindow/GetEqualSubstringWithBudget_1208.java
@@ -0,0 +1,24 @@
+package slidwindow;
+
+/**
+ * 给定两个等长的字符串, 将s 变为t, 每一个字符串的花费是abs(s[i] - t[i]), 最多的花费为maxCost, 问在maxCost 花费内, 可以让st的最大长度为多少.
+ */
+class Solution {
+  public int equalSubstring(String s, String t, int maxCost) {
+    int cost = 0;
+    int le = 0, ri = 0;
+    int ans = 0;
+    while (ri < t.length()) {
+      cost += Math.abs(s.charAt(ri) - t.charAt(ri));
+      while (cost > maxCost) {
+        cost -= Math.abs(s.charAt(le) - t.charAt(le));
+        le++;
+      }
+      ans = Math.max(ans, ri - le + 1);
+      ri++;
+    }
+    return ans;
+  }
+}
+public class GetEqualSubstringWithBudget_1208 {
+}
