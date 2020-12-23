e2275d234926432737a7f36d40364d6bbec8f0f2
kesihai <2693711535@qq.com>
2020-12-23 14:22:54 +0800

    add more problem

---
+++ src/main/java/leetcode/1694.重新格式化电话号码.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1694 lang=cpp
+ *
+ * [1694] 重新格式化电话号码
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string reformatNumber(string number) {
+
+    }
+};
+// @lc code=end
+
