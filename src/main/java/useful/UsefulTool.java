c6e796514bbaab850025911470ad2208d8d0126f
kesihai <2693711535@qq.com>
2021-02-24 09:00:47 +0800

    refine code

--- src/main/java/useful/UsefulTool.java
+++ src/main/java/useful/UsefulTool.java
@@ -1,5 +1,10 @@
 package useful;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class UsefulTool {
   public static void calculateLoan(
       int loanMoney, int year, double loanRatePerYear) {
@@ -39,7 +44,31 @@ public class UsefulTool {
 
 
   public static void main(String[] args) {
-    UsefulTool.calculateLoan(330000, 10, 4.9);
+//    UsefulTool.calculateLoan(330000, 10, 4.9);
+    System.out.println(Arrays.toString(nextGreaterElement(
+        new int[] {4, 2},
+        new int[] {1, 2, 3, 4}
+    )));
+  }
+
+  public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    Stack<Integer> s = new Stack<Integer>();
+    Map<Integer, Integer> mp = new HashMap<>();
+    for (int i = 0; i < nums2.length; i++) {
+      if (s.isEmpty() || nums2[i] <= s.peek()) {
+        s.push(nums2[i]);
+      } else {
+        while (!s.isEmpty() && nums2[i] > s.peek()) {
+          mp.put(s.pop(), nums2[i]);
+        }
+        s.push(nums2[i]);
+      }
+    }
+    int[] ans = new int[nums1.length];
+    for (int i = 0; i < ans.length; i++) {
+      ans[i] = mp.getOrDefault(nums1[i], -1);
+    }
+    return ans;
   }
 }
 
